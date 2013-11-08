8377f05702c94897a0d3c6fd506d4c08b83b68aa
SimonStreicher <streichersj@gmail.com>
2013-11-08 14:54:12 +0200

    Started multivariate PDF in MatLab
    Former-commit-id: 4018085146175f9d58d95303d2db4ee0b7332d04

--- Transfer entropy test case/te_calcs.m
+++ Transfer entropy test case/te_calcs.m
@@ -11,37 +11,41 @@ original_data = original.data;
 load('puredelay');
 puredelay_data = puredelay.data;
 
+c = (4/3)^(1/5); % Constant given in text
+
 % Get PDF for data
 
 [f_original, xi_original] = ksdensity(original_data);
 [f_delay, xi_delay] = ksdensity(puredelay_data);
 
-x_n = length(original_data);
-x_max = max(original_data);
-x_min = min(original_data);
 
+%% Single variable PDF calculation
 % Calculate own PDF using Gaussian Kernel function
 
-% Discrete implementation (Bauer, 2005:135)
+% Discrete implementation (Bauer, 2005:135) - no longer used
 % Using form given by Shu & Zhao (2013:174)
+
+% x_n = length(original_data);
+% x_max = max(original_data);
+% x_min = min(original_data);
 
 % Calculate standard deviation for each data set
 
-sig_original = std(original_data);
-sig_puredelay = std(original_data);
+% sig_original = std(original_data);
+% sig_puredelay = std(original_data);
 
 % Calculate theta for single variable PDF
-c = (4/3)^(1/5); % Constant given in text
-theta_original = c * sig_original * x_n^(-1/5);
 
-%% Single variable PDF calculation
+% theta_original = c * sig_original * x_n^(-1/5);
+
+
 
 % Need to divide range into discrete points
-n_amp = 100; % Number of amplitude bins
-
-x_space = linspace(x_min, x_max, n_amp);
-
-p = zeros(1, n_amp);
+% n_amp = 100; % Number of amplitude bins
+% 
+% x_space = linspace(x_min, x_max, n_amp);
+% 
+% p = zeros(1, n_amp);
 K_sum = 0;
 for k = 1:n_amp   
     for i = 1:x_n
@@ -52,12 +56,70 @@ for k = 1:n_amp
     K_sum = 0;
 end
 
-% Generate discrete grid
+%% Multivariable PDF calculation
 
-% x_discrete = zeros(1, x_n);
-% for i = 1:x_n
-%     x_discrete(i) = round((n_amp - 1) * ((original_data(i) - x_min) / (x_max - x_min))) + 1;
-% end
+% Need to divide range into discrete points
+n_amp = 100; % Number of amplitude bins
+
+% Store data in single variable
+
+data = [original_data, puredelay_data];
+
+% Specify number of varibles
+n_var = length(data(1, :));
+
+% Store length of each variable
+x_n = length(data(:, 1));
+
+% Get maximum and minimum of each variable and generate space
+x_max = zeros(n_var, 1);
+x_min = zeros(n_var, 1);
+x_space = zeros(n_var, n_amp);
+for v = 1:n_var
+    x_max(v, 1) = max(data(:, v));
+    x_min(v, 1) = min(data(:, v));
+    x_space(v, :) = linspace(x_min(v), x_max(v), n_amp);
+end
+
+% Calculate theta for each data set
+theta = zeros(n_var, 1);
+for s = 1:n_var
+    theta(s) = c * std(data(:, s)) * x_n^(-1/(4+s));
+end
+
+p = zeros(n_var, n_amp);
+K_prod = 0;
+K_sum = 0;
+
+for nv = 1:n_var
+ 
+
+
+    for k = 1:n_amp   
+        for i = 1:x_n
+            
+            % Calculate product of kernel functions at a specific bin
+            % reference
+            temp_prod = 1;
+            for p = 1:n_var
+                temp_kernel = single_kernel(x_space(p, k), data(i, p), theta(p));
+                temp_prod = temp_prod * temp_kernel;
+            end
+            
+            temp_sum = single_kernel(x_space(k), original_data(i), theta_original);
+            K_sum = K_sum + temp_sum;
+        end
+        p(k, k)
+        K_sum = 0;
+    end
+    p(k) = (1/x_n) * K_sum;
+end
+
+
+
+
+
+
 
 
 
