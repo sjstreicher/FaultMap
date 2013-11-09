Some bugs fixed - something still wrong in summation Something very fundamental is still wrong in the summation calculation

Transfer entropy test case/TransferEntropy_samplecalc.py
@@ -65,7 +65,7 @@ def vectorselection(data, timelag, samples, k, l):
         # Original form according to Bauer (2007)
 #        x_hist[n-1, :] = data[0, ((sample_n - samples) - timelag * n):(sample_n - timelag * n)]
         # Modified form according to Shu & Zhao (2013)
-        x_hist[n-1, :] = data[0, ((sample_n - samples) - timelag * (n-1) -1):(sample_n - timelag * (n-1))]
+        x_hist[n-1, :] = data[0, ((sample_n - samples) - timelag * (n-1) -1):(sample_n - timelag * (n-1) -1)]
     for m in range(1, (l+1)):
         y_hist[m-1:, :] = data[1, ((sample_n - samples) - timelag * m):(sample_n - timelag * m)]
 
@@ -103,9 +103,13 @@ def te(x_pred, x_hist, y_hist, ampbins):
     [pdf_1, pdf_2, pdf_3, pdf_4] = pdfcalcs(x_pred, x_hist, y_hist)
 
     # Consecutive sums
+
+    # TODO: There is some misunderstanding here...
     tesum = 0
     for s1 in x_pred_space:
+        print s1
         for s2 in x_hist_space:
+#            print s2
             for s3 in y_hist_space:
                 sum_element = tecalc(pdf_1, pdf_2, pdf_3, pdf_4, s1, s2, s3)
                 tesum = tesum + sum_element
@@ -117,26 +121,26 @@ def pdfcalcs(x_pred, x_hist, y_hist):
     """Calculates the PDFs required to calculate transfer entropy"""
 
     # Get dimensions of vectors
-    k = np.size(x_hist[:, 1])
-    l = np.size(y_hist[:, 1])
+#    k = np.size(x_hist[:, 1])
+#    l = np.size(y_hist[:, 1])
 
     # Currently only works for k = 1; l = 1
     # TODO: Generalize for k and l
 
     # Calculate p(x_{i+h}, x_i, y_i)
-    data_1 = np.vstack([x_pred, x_hist[1, :], y_hist[1, :]])
+    data_1 = np.vstack([x_pred, x_hist[0, :], y_hist[0, :]])
     pdf_1 = stats.gaussian_kde(data_1, 'silverman')
 
     # Calculate p(x_i, y_i)
-    data_2 = np.vstack([x_hist[1, :], y_hist[1, :]])
+    data_2 = np.vstack([x_hist[0, :], y_hist[0, :]])
     pdf_2 = stats.gaussian_kde(data_2, 'silverman')
 
     # Calculate p(x_{i+h}, x_i)
-    data_3 = np.vstack([x_pred, x_hist[1, :]])
+    data_3 = np.vstack([x_pred, x_hist[0, :]])
     pdf_3 = stats.gaussian_kde(data_3, 'silverman')
 
     # Calculate p(x_i)
-    data_4 = x_hist[1, :]
+    data_4 = x_hist[0, :]
     pdf_4 = stats.gaussian_kde(data_4, 'silverman')
 
     return pdf_1, pdf_2, pdf_3, pdf_4
@@ -145,9 +149,12 @@ def pdfcalcs(x_pred, x_hist, y_hist):
 def tecalc(pdf_1, pdf_2, pdf_3, pdf_4, x_pred_val, x_hist_val, y_hist_val):
     """Calculate elements for summation for a specific set of coordinates"""
     logterm_num = (pdf_1([x_pred_val, x_hist_val, y_hist_val]) /
-                   pdf_2([x_pred_val, x_hist_val]))
+                   pdf_2([x_hist_val, y_hist_val]))
+
     logterm_den = pdf_3([x_pred_val, x_hist_val]) / pdf_4([x_hist_val])
+
     coeff = pdf_1([x_pred_val, x_hist_val, y_hist_val])
+
     sum_element = coeff * np.log(logterm_num / logterm_den)
 
     return sum_element
@@ -157,7 +164,7 @@ def tecalc(pdf_1, pdf_2, pdf_3, pdf_4, x_pred_val, x_hist_val, y_hist_val):
 
 [x_pred, x_hist, y_hist] = vectorselection(data, 100, 3000, 1, 1)
 
-TE = te(x_pred, x_hist, y_hist, 100)
+TE = te(x_pred, x_hist, y_hist, 10)
 
 #[pdf_1, pdf_2, pdf_3, pdf_4] = pdfcalcs(x_pred, x_hist, y_hist)
 