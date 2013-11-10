b3c751eabbf7cc31254e82b1def658c8e2b551af
SimonStreicher <streichersj@gmail.com>
2013-11-10 13:24:45 +0200

    Some attempts made to handle zero or negative PDF evaluations
    Former-commit-id: 43792b2b563768051bafc643ee5c3b4355a0ff1a

--- Transfer entropy test case/csv_export.m
+++ Transfer entropy test case/csv_export.m
@@ -6,16 +6,16 @@ clc
 
 load('original');
 original_data = original.data;
-
 load('puredelay');
 puredelay_data = puredelay.data;
-
 load('delayedtf');
+load('puretf');
 load('doubledelayedtf');
 
 csvwrite('original_data.csv', original_data(1:end));
 csvwrite('puredelay_data.csv', puredelay_data(1:end));
 csvwrite('delayedtf_data.csv', delayedtf.data(1:end));
 csvwrite('doubledelayedf_data.csv', doubledelayedtf.data(1:end));
+csvwrite('puretf_data.csv', puretf.data(1:end));
 
 
