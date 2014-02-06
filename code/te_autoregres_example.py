9c87e15943d216af6e69f9af37a0034e9f4c32d6
SimonStreicher <streichersj@gmail.com>
2014-02-06 15:41:10 +0200

    Code running again, but not correct
    Former-commit-id: 6130a81678b86f7888e5a1c5d3b0edd92a4461ba

--- code/te_autoregres_example.py
+++ code/te_autoregres_example.py
@@ -4,8 +4,71 @@ Created on Thu Feb 06 10:50:28 2014
 @author: Simon Streicher
 """
 
-# Generate autoretgressive delayed data vectors
+from numpy import loadtxt, vstack
+import numpy as np
+from autoregres_gen import autogen
+from transfer_entropy import vectorselection, te
 
-autoregres_gen
+
+def getdata(samples, delay):
+    """Get dataset for testing.
+
+    Select to generate each run or import an existing dataset.
+
+    """
+
+    # Generate autoregressive delayed data vectors internally
+    data = autogen(samples, delay)
+
+    # Alternatively, import data from file
+#    autoregx = loadtxt('autoregx_data.csv')
+#    autoregy = loadtxt('autoregy_data.csv')
+
+    return data
 
 
+def calculate_te(delay, timelag, samples, sub_samples, ampbins, k=1, l=1):
+    """Calculates the transfer entropy for a specific timelag (equal to
+    prediction horison) for a set of autoregressive data.
+
+    sub_samples is the amount of samples in the dataset used to calculate the
+    transfer entropy between two vectors (taken from the end of the dataset).
+    sub_samples <= samples
+
+    Currently only supports k = 1; l = 1;
+
+    You can search through a set of timelags in an attempt to identify the
+    original delay.
+    The transfer entropy should have a maximum value when timelag = delay
+    used to generate the autoregressive dataset.
+
+    """
+    # Get autoregressive datasets
+    data = getdata(samples, delay)
+
+    [x_pred, x_hist, y_hist] = vectorselection(data, timelag,
+                                               sub_samples, k, l)
+
+    tentropy = te(x_pred, x_hist, y_hist, ampbins)
+
+    return tentropy
+
+# Test code
+# Delay = 5, Timelag = 4
+tentropy1 = calculate_te(5, 4, 1000, 500, 10)
+# Delay = 5, Timelag = 5
+tentropy2 = calculate_te(5, 5, 1000, 500, 10)
+# Delay = 5, Timelag = 6
+tentropy3 = calculate_te(5, 6, 1000, 500, 10)
+
+
+#samples = 30
+#delay = 5
+#
+#data = autogen(samples, delay)
+#x = data[0]
+#y = data[1]
+#
+#x_diff = np.zeros_like(x[delay-1:-1])
+#for i in range(delay, len(data[0])):
+#    x_diff[i - delay] = x[i] - x[i - 1]
--- code/transfer_entropy.py
+++ code/transfer_entropy.py
@@ -1,7 +1,7 @@
 """
 Created on Fri Nov 08 15:37:47 2013
 
-@author: s13071832
+@author: Simon Streicher
 
 """
 
@@ -13,94 +13,68 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-def te(x_pred, x_hist, y_hist, ampbins):
-    """Calculates the transfer entropy between two variables from a set of
-    vectors already calculated.
+def vectorselection(data, timelag, sub_samples, k, l):
+    """Generates sets of vectors for calculating transfer entropy.
 
-    ampbins is the number of amplitude bins to use over each variable
+    For notation references see Shu2013.
+
+    Takes into account the time lag (number of samples between vectors of the
+    same variable).
+
+    In this application the prediction horizon (h) is set to equal
+    to the time lag.
+
+    The first vector in the data array should be the samples of the variable
+    to be predicted (x) while the second vector should be sampled of the vector
+    used to make the prediction (y).
+
+    sub_samples is the amount of samples in the dataset used to calculate the
+    transfer entropy between two vectors.
+    The required number of samples is extracted from the end of the vector.
+    If the vector is longer than the number of samples specified plus the
+    desired time lag then the remained of the data will be discarded.
+    sub_samples <= samples
+
+
+    k refers to the dimension of the historical data to be predicted (x)
+
+    l refers to the dimension of the historical data used
+    to do the prediction (y)
 
     """
+    _, sample_n = data.shape
+    x_pred = data[0, :-sub_samples]
 
-    # First do an example for the case of k = l = 1
-    # TODO: Sum loops to allow for a general case
+    x_hist = np.zeros((k, sub_samples))
+    y_hist = np.zeros((l, sub_samples))
 
-    # Divide the range of each variable into amplitude bins to sum over
-    x_pred_min = x_pred.min()
-    x_pred_max = x_pred.max()
-    x_hist_min = x_hist.min()
-    x_hist_max = x_hist.max()
-    y_hist_min = y_hist.min()
-    y_hist_max = y_hist.max()
+    for n in range(1, (k+1)):
+        # Original form according to Bauer (2007)
+#        x_hist[n-1, :] = data[0, ((sample_n - samples) - timelag * n):
+#                               (sample_n - timelag * n)]
+        # Modified form according to Shu & Zhao (2013)
+        x_hist[n-1, :] = data[0, ((sample_n - sub_samples) - timelag *
+                                  (n-1) - 1):(sample_n - timelag * (n-1) - 1)]
+    for m in range(1, (l+1)):
+        y_hist[m-1:, :] = data[1, ((sample_n - sub_samples) - timelag * m):
+                               (sample_n - timelag * m)]
 
-    x_pred_space = np.linspace(x_pred_min, x_pred_max, ampbins)
-    x_hist_space = np.linspace(x_hist_min, x_hist_max, ampbins)
-    y_hist_space = np.linspace(y_hist_min, y_hist_max, ampbins)
+#    for n in range(1, (k+1)):
+#        x_hist = data[0, ((sample_n - samples) - timelag * n):
+#                            (sample_n - timelag * n)]
+#    for m in range(1, (l+1)):
+#        y_hist = data[1, ((sample_n - samples) - timelag * m):
+#                            (sample_n - timelag * m)]
 
-    x_pred_diff = x_pred_space[1] - x_pred_space[0]
-    x_hist_diff = x_hist_space[1] - x_hist_space[0]
-    y_hist_diff = y_hist_space[1] - y_hist_space[0]
-
-    # Calculate PDFs for all combinations required
-    [pdf_1, pdf_2, pdf_3, pdf_4] = pdfcalcs(x_pred, x_hist, y_hist)
-
-    # Consecutive sums
-    # TODO: Make sure Riemann sum diff elements is handled correctly
-
-    tesum = 0
-#    tesum_old = -1
-#    sumelement_store = np.zeros(ampbins**3)
-    delement = x_pred_diff * x_hist_diff * y_hist_diff
-    print delement
-    for s1 in x_pred_space:
-#        print 's1', s1
-        for s2 in x_hist_space:
-#            print 's2', s2
-            for s3 in y_hist_space:
-#                print 's3', s3
-                sum_element = tecalc(pdf_1, pdf_2, pdf_3, pdf_4, s1, s2, s3)
-                tesum = tesum + sum_element
-                # Try to detect point at which the huge term enters
-                # For special case of data = np.vstack([puretf, original])
-                # and timelag = 1 and ampbins = 20
-                # Has to do with a huge value for PDF1 at a specific point
-#                if tesum / tesum_old < 0:
-#                    print s1, s2, s3
-#                    temp1 = pdf_1([s1, s2, s3])
-#                    temp2 = pdf_2([s2, s3])
-#                    temp3 = pdf_3([s1, s2])
-#                    temp4 = pdf_4([s2])
-#                    print temp1, temp2, temp3, temp4
-#                tesum_old = tesum
-#                print tesum
-#                sumelement_store[s1*s2*s3] = sum_element
-    te = tesum * delement
-
-    # Using local sums
-    # (It does give the same result)
-
-#    sums3 = 0
-#    sums2 = 0
-#    sums1 = 0
-#    for s1 in x_pred_space:
-#        print s1
-#        sums2 = 0
-#        for s2 in x_hist_space:
-##            print s2
-#            sums3 = 0
-#            for s3 in y_hist_space:
-#                sum_element = tecalc(pdf_1, pdf_2, pdf_3, pdf_4, s1, s2, s3)
-#                sums3 = sums3 + sum_element
-#            sums2 = sums2 + sums3 * y_hist_diff
-#        sums1 = sums1 + sums2 * x_hist_diff
-#        te = sums1 * x_pred_diff
-
-    return te
+    return x_pred, x_hist, y_hist
 
 
 def pdfcalcs(x_pred, x_hist, y_hist):
-    """Calculates the PDFs required to calculate transfer entropy."""
+    """Calculates the PDFs required to calculate transfer entropy.
 
-    # Currently only works for k = 1; l = 1
+    Currently only supports k = 1; l = 1
+
+    """
     # TODO: Generalize for k and l
 
     # Get dimensions of vectors
@@ -126,7 +100,8 @@ def pdfcalcs(x_pred, x_hist, y_hist):
     return pdf_1, pdf_2, pdf_3, pdf_4
 
 
-def tecalc(pdf_1, pdf_2, pdf_3, pdf_4, x_pred_val, x_hist_val, y_hist_val):
+def te_elementcalc(pdf_1, pdf_2, pdf_3, pdf_4, x_pred_val,
+                   x_hist_val, y_hist_val):
     """Calculate elements for summation for a specific set of coordinates"""
 
     # Need to find a proper way to correct for cases when PDFs return 0
@@ -179,19 +154,105 @@ def tecalc(pdf_1, pdf_2, pdf_3, pdf_4, x_pred_val, x_hist_val, y_hist_val):
         sum_element = 0
 
 #        print logterm_num, logterm_den
-    print sum_element
+#    print sum_element
 
     return sum_element
 
 
-"""Testing commands"""
+def te(x_pred, x_hist, y_hist, ampbins):
+    """Calculates the transfer entropy between two variables from a set of
+    vectors already calculated.
 
-[x_pred, x_hist, y_hist] = vectorselection(data, 4, 3000, 1, 1)
+    ampbins is the number of amplitude bins to use over each variable
 
-TE = te(x_pred, x_hist, y_hist, 25)
-print 'TE: ', float(TE)
+    """
 
-[pdf_1, pdf_2, pdf_3, pdf_4] = pdfcalcs(x_pred, x_hist, y_hist)
+    # First do an example for the case of k = l = 1
+    # TODO: Sum loops to allow for a general case
+
+    # Divide the range of each variable into amplitude bins to sum over
+
+    x_pred_min = x_pred.min()
+    x_pred_max = x_pred.max()
+    x_hist_min = x_hist.min()
+    x_hist_max = x_hist.max()
+    y_hist_min = y_hist.min()
+    y_hist_max = y_hist.max()
+
+    x_pred_space = np.linspace(x_pred_min, x_pred_max, ampbins)
+    x_hist_space = np.linspace(x_hist_min, x_hist_max, ampbins)
+    y_hist_space = np.linspace(y_hist_min, y_hist_max, ampbins)
+
+    x_pred_diff = x_pred_space[1] - x_pred_space[0]
+    x_hist_diff = x_hist_space[1] - x_hist_space[0]
+    y_hist_diff = y_hist_space[1] - y_hist_space[0]
+
+    # Calculate PDFs for all combinations required
+    [pdf_1, pdf_2, pdf_3, pdf_4] = pdfcalcs(x_pred, x_hist, y_hist)
+
+    # Consecutive sums
+    # TODO: Make sure Riemann sum diff elements is handled correctly
+
+    tesum = 0
+#    tesum_old = -1
+#    sumelement_store = np.zeros(ampbins**3)
+    delement = x_pred_diff * x_hist_diff * y_hist_diff
+    print delement
+    for s1 in x_pred_space:
+#        print 's1', s1
+        for s2 in x_hist_space:
+#            print 's2', s2
+            for s3 in y_hist_space:
+#                print 's3', s3
+                sum_element = te_elementcalc(pdf_1, pdf_2, pdf_3, pdf_4,
+                                             s1, s2, s3)
+                tesum = tesum + sum_element
+                # Try to detect point at which the huge term enters
+                # For special case of data = np.vstack([puretf, original])
+                # and timelag = 1 and ampbins = 20
+                # Has to do with a huge value for PDF1 at a specific point
+#                if tesum / tesum_old < 0:
+#                    print s1, s2, s3
+#                    temp1 = pdf_1([s1, s2, s3])
+#                    temp2 = pdf_2([s2, s3])
+#                    temp3 = pdf_3([s1, s2])
+#                    temp4 = pdf_4([s2])
+#                    print temp1, temp2, temp3, temp4
+#                tesum_old = tesum
+#                print tesum
+#                sumelement_store[s1*s2*s3] = sum_element
+    te = tesum * delement
+
+    # Using local sums
+    # (It does give the same result)
+
+#    sums3 = 0
+#    sums2 = 0
+#    sums1 = 0
+#    for s1 in x_pred_space:
+#        print s1
+#        sums2 = 0
+#        for s2 in x_hist_space:
+##            print s2
+#            sums3 = 0
+#            for s3 in y_hist_space:
+#                sum_element = tecalc(pdf_1, pdf_2, pdf_3, pdf_4, s1, s2, s3)
+#                sums3 = sums3 + sum_element
+#            sums2 = sums2 + sums3 * y_hist_diff
+#        sums1 = sums1 + sums2 * x_hist_diff
+#        te = sums1 * x_pred_diff
+
+    return te
+
+
+#"""Testing commands"""
+#
+#[x_pred, x_hist, y_hist] = vectorselection(data, 4, 3000, 1, 1)
+#
+#TE = te(x_pred, x_hist, y_hist, 25)
+#print 'TE: ', float(TE)
+#
+#[pdf_1, pdf_2, pdf_3, pdf_4] = pdfcalcs(x_pred, x_hist, y_hist)
 
 # Plot to see why the PDFs fail repeatedly
 
