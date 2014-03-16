5e640127aef4fd5d1ee0b24276e84820be4f80c8
Simon Streicher <streichersj@gmail.com>
2014-03-16 15:54:30 +0200

    Added threshold tests

--- ranking/gaincalc.py
+++ ranking/gaincalc.py
@@ -62,8 +62,9 @@ def calc_partialcor_gainmatrix(connectionmatrix, tags_tsdata, dataset):
     return correlationmatrix, partialcorrelationmatrix
 
 
-def calc_max_partialcor(variables, connectionmatrix, inputdata,
-                        dataset, sampling_rate, delays, size, delaytype):
+def calc_max_partialcorr_delay(variables, connectionmatrix, inputdata,
+                               dataset, sampling_rate, delays,
+                               size, delaytype):
     """Determines the maximum partial correlation between two variables.
 
     size refers to the number of elements of two time series data vectors used
@@ -74,15 +75,17 @@ def calc_max_partialcor(variables, connectionmatrix, inputdata,
 
     """
     logging.basicConfig(level=logging.INFO)
-    max_val_array = np.empty((len(variables), len(variables)))
-    max_delay_array = np.empty((len(variables), len(variables)))
-    max_val_array[:] = np.NAN
-    max_delay_array[:] = np.NAN
+    corr_array = np.empty((len(variables), len(variables)))
+    delay_array = np.empty((len(variables), len(variables)))
+    corr_array[:] = np.NAN
+    delay_array[:] = np.NAN
 
     data_header = ['causevar', 'affectedvar', 'base_corr',
-                   'max_corr', 'max_delay',
-                   'max_index', 'min_corr', 'min_delay', 'min_index',
-                   'signchange']
+                   'max_corr', 'max_delay', 'max_index',
+                   'signchange', 'corrthreshpass', 'dirrthreshpass']
+
+    threshcorr = (1.85*(size**(-0.41))) + (2.37*(size**(-0.53)))
+    threshdir = 0.46*(size**(-0.16))
 
     datastore = []
     if delaytype == 'datapoints':
@@ -110,48 +113,67 @@ def calc_max_partialcor(variables, connectionmatrix, inputdata,
                         np.corrcoef(causevardata.T, affectedvardata.T)[1, 0]
                     corrlist.append(corrval)
 
-                corrlist_abs = [abs(val) for val in corrlist]
-                max_value_abs = max(corrlist_abs)
-                min_value_abs = min(corrlist_abs)
-                max_index = corrlist_abs.index(max_value_abs)
-                max_value = corrlist[max_index]
-                min_index = corrlist_abs.index(min_value_abs)
-                min_value = corrlist[min_index]
-                max_delay = actual_delays[max_index]
-                min_delay = actual_delays[min_index]
-                max_val_array[affectedvarindex, causevarindex] = max_value
-                max_delay_array[affectedvarindex, causevarindex] = max_delay
+                maxval = max(corrlist)
+                minval = min(corrlist)
+                if (maxval + minval) >= 0:
+                    delay_index = corrlist.index(maxval)
+                    maxcorr = maxval
+                else:
+                    delay_index = corrlist.index(minval)
+                    maxcorr = minval
+                # Bauer2008 eq. 4
+                maxcorr_abs = max(maxval, abs(minval))
+                bestdelay = actual_delays[delay_index]
+                directionindex = 2 * (abs(maxval + minval) /
+                                      (maxval + abs(minval)))
+                corr_array[affectedvarindex, causevarindex] = maxcorr
+                delay_array[affectedvarindex, causevarindex] = bestdelay
 
-                if (corrlist[0] / corrlist[max_index]) >= 0:
+                if (corrlist[0] / corrlist[delay_index]) >= 0:
                     signchange = False
                 else:
                     signchange = True
 
+                if maxcorr_abs >= threshcorr:
+                    corrthreshpass = True
+                else:
+                    corrthreshpass = False
+                    # Don't use the shifted correlation
+
+                if directionindex >= threshdir:
+                    dirthreshpass = True
+                else:
+                    dirthreshpass = False
+                    # Don't use the shifted correlatio
+
                 dataline = [causevar, affectedvar, str(corrlist[0]),
-                            max_value, str(max_delay*3600), str(max_index),
-                            str(min_value), str(min_delay*3600),
-                            str(min_index), signchange]
+                            maxcorr, str(bestdelay*3600), str(delay_index),
+                            signchange, corrthreshpass, dirthreshpass]
 
                 datastore.append(dataline)
-
-                logging.info("The maximum correlation between " + causevar
-                             + " and " + affectedvar + " is at a delay of: " +
-                             str(max_delay*3600) +
-                             " seconds, delay index number: " + str(max_index))
-                logging.info("The maximum correlation is: " + str(max_value) +
-                             " while the minimum correlation is: " +
-                             str(min_value) +
-                             " which occurs at a delay of: " +
-                             str(min_delay*3600) +
-                             " seconds, delay index number: " + str(min_index))
+                logging.info("Maximum correlation value: " + str(maxval))
+                logging.info("Minimum correlation value: " + str(minval))
+                logging.info("The maximum correlation between " + causevar +
+                             " and " + affectedvar + " is: " + str(maxcorr))
+                logging.info("The corresponding delay is: " +
+                             str(bestdelay*3600) +
+                             " seconds, delay index number: " +
+                             str(delay_index))
                 logging.info("The correlation with no delay is: "
                              + str(corrlist[0]))
+#                logging.info("Correlation threshold: " + str(threshcorr))
+                logging.info("Correlation threshold passed: " +
+                             str(corrthreshpass))
+#                logging.info("Directionbality threshold: " + str(threshdir))
+                logging.info("Directionality value: " + str(directionindex))
+                logging.info("Directionality threshold passed: " +
+                             str(dirthreshpass))
 
             else:
-                max_val_array[affectedvarindex, causevarindex] = np.NAN
-                max_delay_array[affectedvarindex, causevarindex] = np.NAN
+                corr_array[affectedvarindex, causevarindex] = np.NAN
+                delay_array[affectedvarindex, causevarindex] = np.NAN
 
-    return max_val_array, max_delay_array, datastore, data_header
+    return corr_array, delay_array, datastore, data_header
 
 
 def calc_transentropy_gainmatrix(connectionmatrix, tags_tsdata):
