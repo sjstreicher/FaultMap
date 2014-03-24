68c0ab85c839fc1c33e51f2436f6978c865adf1a
Simon Streicher <streichersj@gmail.com>
2014-03-24 14:45:46 +0200

    Removed duplication from demo_setup.py

--- demo_setup.py
+++ demo_setup.py
@@ -27,20 +27,4 @@ def runsetup():
     # Get sampling rate
     sampling_rate = caseconfig['sampling_rate']
 
-    # Load directories config file
-    dirs = json.load(open('config.json'))
-    # Get data and preferred export directories from directories config file
-    dataloc = os.path.expanduser(dirs['dataloc'])
-    saveloc = os.path.expanduser(dirs['saveloc'])
-    # Define plant and case names to run
-    plant = 'tennessee_eastman'
-    # Define plant data directory
-    plantdir = os.path.join(dataloc, 'plants', plant)
-    cases = ['dist11_closedloop', 'dist11_closedloop_pressup', 'dist11_full',
-             'presstep_closedloop', 'presstep_full']
-    # Load plant config file
-    caseconfig = json.load(open(os.path.join(plantdir, plant + '.json')))
-    # Get sampling rate
-    sampling_rate = caseconfig['sampling_rate']
-
     return cases, saveloc, caseconfig, plantdir, sampling_rate, infodynamicsloc
