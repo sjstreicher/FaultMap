7eb21c89a9aabb0f91e3100bbf9a2639d0f1f6c5
Simon Streicher <streichersj@gmail.com>
2014-03-26 09:27:48 +0200

    Eliminated duplicate

--- demo_estimate_delay.py
+++ demo_estimate_delay.py
@@ -13,6 +13,9 @@ Created on Thu Mar 13 09:44:16 2014
 from demo_setup import runsetup
 from ranking.gaincalc import create_connectionmatrix
 from ranking.gaincalc import estimate_delay
+# Import all test data generator functions that may be called
+# via the config file
+from datagen import *
 
 from sklearn import preprocessing
 
@@ -39,20 +42,24 @@ delaytype = 'datapoints'
 delays = []
 
 # Specify method either as 'partial_correlation' or 'transfer_entropy'
-# Store this in the case configuration file
+# TODO: Store this in the case configuration file
 method = 'transfer_entropy'
 #method = 'partial_correlation'
 logging.info("Method: " + method)
 
 if delaytype == 'datapoints':
 # Include first n sampling intervals
-    delays = range(10)
+    delays = range(11)
 elif delaytype == 'timevalues':
 # Include first n 10-second shifts
     delays = [val * (10.0/3600.0) for val in range(1000)]
 
+#datatype = 'function'
+datatype = 'file'
+
 # Value chosen for demonstration purposes only
-size = 400
+# Similar to subsamples in vectorselection function
+size = 2000
 
 if method == 'transfer_entropy':
     # Change location of jar to match yours:
@@ -63,27 +70,33 @@ if method == 'transfer_entropy':
     jpype.startJVM(jpype.getDefaultJVMPath(), "-ea",
                    "-Djava.class.path=" + jarLocation)
 
-# Only do a single case in the demo for coverage analysis purposes
-for case in cases:
+# Only running first case while still debugging
+for case in [cases[0]]:
     logging.info("Running case {}".format(case))
     # Get connection (adjacency) matrix
     connectionloc = os.path.join(plantdir, 'connections',
                                  caseconfig[case]['connections'])
     # Get time series data
-    tags_tsdata = os.path.join(plantdir, 'data', caseconfig[case]['data'])
-    # Get dataset name
-    dataset = caseconfig[case]['dataset']
-    inputdata = np.array(h5py.File(tags_tsdata, 'r')[dataset])
+    if datatype == 'file':
+        tags_tsdata = os.path.join(plantdir, 'data', caseconfig[case]['data'])
+        # Get dataset name
+        dataset = caseconfig[case]['dataset']
+        inputdata = np.array(h5py.File(tags_tsdata, 'r')[dataset])
+    elif datatype == 'function':
+        tags_tsdata_gen = caseconfig[case]['datagen']
+        samples = 5000
+        delay = 5
+        inputdata = eval(tags_tsdata_gen)(samples, delay)
+
     # Normalise (mean centre and variance scale) the input data
-    inputdata_norm = preprocessing.scale(inputdata)
+    inputdata_norm = preprocessing.scale(inputdata, axis=0)
 
     # Get the variables and connection matrix
     [variables, connectionmatrix] = create_connectionmatrix(connectionloc)
 
     [weight_array, delay_array, datastore, data_header] = \
         estimate_delay(variables, connectionmatrix, inputdata_norm,
-                       dataset, sampling_rate, delays,
-                       size, delaytype, method, infodynamicsloc)
+                       sampling_rate, size, delays, delaytype, method)
 
     # Define export directories and filenames
     datasavename = \
@@ -102,3 +115,7 @@ for case in cases:
 
     # Write datastore to file
     writecsv(datasavename, datastore, data_header)
+
+if method == 'transfer_entropy':
+    # Shutdown the JVM
+    jpype.shutdownJVM()
