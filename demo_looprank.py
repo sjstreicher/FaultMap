36ea6e3aafe8b8fc5cd29227abb8132be537e7fe
Simon Streicher <streichersj@gmail.com>
2014-03-29 14:08:13 +0200

    Structural changes

--- demo_looprank.py
+++ demo_looprank.py
@@ -9,8 +9,8 @@ from ranking.gaincalc import create_connectionmatrix
 from ranking.gaincalc import calc_partialcor_gainmatrix
 from ranking.formatmatrices import rankforward, rankbackward
 from ranking.formatmatrices import split_tsdata
-from ranking.noderank import calculate_rank
-from ranking.noderank import create_blended_ranking
+from ranking.noderank import calc_simple_rank
+from ranking.noderank import calc_blended_rank
 from ranking.noderank import calc_transient_importancediffs
 from ranking.noderank import plot_transient_importances
 from ranking.noderank import create_importance_graph
@@ -25,57 +25,108 @@ import logging
 logging.basicConfig(level=logging.INFO)
 
 mode = 'test_cases'
-case = 'autoreg_2x2'
+case = 'series'
+
+# Define the method for determining the gainmatrix
+# TODO: Include this in the case configuration file
+#method = 'transfer_entropy'
+method = 'partial_correlation'
 
 # Optional methods
+# Do a single ranking on all data
+singleranking = True
+# Do a transient ranking on all data boxes
+transientranking = False
 # Save plots of transient rankings
-transientplots = True
-importancegraph = True
+transientplots = False
 
-scenarios, saveloc, scenconfig, casedir, sampling_rate, \
+scenarios, saveloc, caseconfig, casedir, sampling_rate, \
     infodynamicsloc, datatype = runsetup(mode, case)
-
-openconnectionloc = os.path.join(casedir, 'connections',
-                                 scenconfig['open_connections'])
-[_, openconnectionmatrix] = create_connectionmatrix(openconnectionloc)
-
 
 def writecsv(filename, items):
     with open(filename, 'wb') as f:
         csv.writer(f).writerows(items)
 
-
-def gainrank(gainmatrix, variables, connectionmatrix):
+def gainrank(gainmatrix, variables, connectionmatrix,
+             alpha=0.50, dummyweight=1.0):
     # TODO: The forward, backward and blended ranking will all be folded
     # into a single method, currently isolated for ease of access to
     # intermediate results
     forwardconnection, forwardgain, forwardvariablelist = \
-        rankforward(variables, gainmatrix, connectionmatrix, 0.01)
+        rankforward(variables, gainmatrix, connectionmatrix, dummyweight)
+        
     backwardconnection, backwardgain, backwardvariablelist = \
-        rankbackward(variables, gainmatrix, connectionmatrix, 0.01)
-    forwardrank = calculate_rank(forwardgain, forwardvariablelist)
-    backwardrank = calculate_rank(backwardgain, backwardvariablelist)
-    rankingdict, slist = create_blended_ranking(forwardrank, backwardrank,
-                                                variables, alpha=0.35)
-    return rankingdict, slist
+        rankbackward(variables, gainmatrix, connectionmatrix, dummyweight)
+        
+    forwardrankingdict, forwardrankinglist = \
+        calc_simple_rank(forwardgain, forwardvariablelist)
+    
+    backwardrankingdict, backwardrankinglist = \
+        calc_simple_rank(backwardgain, backwardvariablelist)
+        
+    blendedrankingdict, blendedrankinglist = \
+        calc_blended_rank(forwardrankingdict, backwardrankingdict,
+                          variables, alpha)
+    
+    rankingdicts = [blendedrankingdict, forwardrankingdict, backwardrankingdict]    
+    rankinglists = [blendedrankinglist, forwardrankinglist, backwardrankinglist] 
+    
+    return rankingdicts, rankinglists
 
 
-def looprank_single(scenario, variables, connectionmatrix,
-                    tags_tsdata, dataset):
-    # Get the correlation and partial correlation matrices
-    _, gainmatrix = \
-        calc_partialcor_gainmatrix(connectionmatrix, tags_tsdata, dataset)
+def calc_gainmatrix(connectionmatrix, tags_tsdata, dataset,
+                   method='partial_correlation'):
+    """Calculates the gainmatrix from tags time series data.
+    
+    Can make use of either the partial correlation or transfer entropy method
+    for determining weights.
+    
+    """
+    
+    if method == 'partial_correlation':
+        # Get the partialcorr gainmatrix
+        _, gainmatrix = \
+            calc_partialcor_gainmatrix(connectionmatrix, tags_tsdata, dataset)
+            
+    elif method == 'transfer_entropy':
+        # TODO: Implement transfer entropy weight calculation
+        gainmatrix = None
+    
     savename = os.path.join(saveloc, "gainmatrix.csv")
     np.savetxt(savename, gainmatrix, delimiter=',')
+     
+    return gainmatrix
+    
+    
+def looprank_single(scenario, variables, connectionmatrix, gainmatrix):
+    """Ranks the nodes in a network given a gainmatrix, connectionmatrix
+    and a list of variables.
+    
+    """
+    
+    # TODO: Refactor some more main loop functions here
+    
+    rankingdicts, rankinglists = gainrank(gainmatrix, variables,
+                                          connectionmatrix)                          
+    
+    directions = ['blended', 'forward', 'backward']                                    
+    for direction, rankinglist, rankingdict in zip(directions, rankinglists, rankingdicts):
+        # Save the ranking list to file        
+        savename = os.path.join(saveloc, scenario + '_{}_importances.csv'.format(direction))
+        writecsv(savename, rankinglist)
+        # Save the graphs to file
+        closedgraph, _ = \
+                create_importance_graph(variables, connectionmatrix,
+                                        connectionmatrix, gainmatrix,
+                                        rankingdict)
+        closedgraph_filename = os.path.join(saveloc,
+                                            "{}_{}_closedgraph.gml".format(scenario, direction))
 
-    rankingdict, rankinglist = gainrank(gainmatrix, variables,
-                                        connectionmatrix)
-
-    savename = os.path.join(saveloc, scenario + '_importances.csv')
-    writecsv(savename, rankinglist)
+        nx.readwrite.write_gml(closedgraph, closedgraph_filename)
+   
     logging.info("Done with single ranking")
-
-    return gainmatrix, rankingdict
+    
+    return None
 
 
 def looprank_transient(scenario, samplerate, boxsize, boxnum, variables,
@@ -109,57 +160,60 @@ def looprank_transient(scenario, samplerate, boxsize, boxnum, variables,
 
     transientdict, basevaldict = \
         calc_transient_importancediffs(rankingdicts, variables)
+    
+    # Plotting functions
+    diffplot, absplot = plot_transient_importances(variables,
+                                                   transientdict,
+                                                   basevaldict)
+    diffplot_filename = os.path.join(saveloc,
+                                     "{}_diffplot.pdf".format(scenario))
+    absplot_filename = os.path.join(saveloc,
+                                    "{}_absplot.pdf".format(scenario))
+    diffplot.savefig(diffplot_filename)
+    absplot.savefig(absplot_filename)
 
     logging.info("Done with transient rankings")
 
-    return transientdict, basevaldict
+    return None
 
 for scenario in scenarios:
-    # Get connection (adjacency) matrix
     logging.info("Running scenario {}".format(scenario))
-    # Get connection (adjacency) matrix
-    connectionloc = os.path.join(casedir, 'connections',
-                                 scenconfig[scenario]['connections'])
+    if datatype == 'file':
+        # Get connection (adjacency) matrix
+        connectionloc = os.path.join(casedir, 'connections',
+                                     caseconfig[scenario]['connections'])
+        # Get the variables and closedloop connection matrix
+        [variables, connectionmatrix] = create_connectionmatrix(connectionloc)
+        # Get the openloop connectionmatrix
+        openconnectionloc = os.path.join(casedir, 'connections',
+                                 caseconfig['open_connections'])
+        [_, openconnectionmatrix] = create_connectionmatrix(openconnectionloc)
 
-    # Get time series data
-    tags_tsdata = os.path.join(casedir, 'data', scenconfig[scenario]['data'])
-    # Get dataset name
-    dataset = scenconfig[scenario]['dataset']
-    # Get the variables and connection matrix
-    [variables, connectionmatrix] = create_connectionmatrix(connectionloc)
+        # Get time series data
+        tags_tsdata = os.path.join(casedir, 'data', caseconfig[scenario]['data'])
+        # Get dataset name
+        dataset = caseconfig[scenario]['dataset']
+        
+        # Calculate the gainmatrix
+        gainmatrix = calc_gainmatrix(connectionmatrix, tags_tsdata, dataset)
+        
+        boxnum = caseconfig[scenario]['boxnum']
+        boxsize = caseconfig[scenario]['boxsize']
+        
+    elif datatype == 'function':
+        # Get variables, connection matrix and gainmatrix
+        # For the test cases the openloop connection matrix is kept identical
+        # to the closedloop connection matrix
+        network_gen = caseconfig[scenario]['networkgen']
+        connectionmatrix, gainmatrix, variables, _ = eval(network_gen)
 
     logging.info("Number of tags: {}".format(len(variables)))
-    boxnum = scenconfig[scenario]['boxnum']
-    boxsize = scenconfig[scenario]['boxsize']
 
-    gainmatrix, rankingdict = looprank_single(scenario, variables,
-                                              connectionmatrix,
-                                              tags_tsdata, dataset)
-
-    [transientdict, basevaldict] = \
-        looprank_transient(scenario, sampling_rate, boxsize, boxnum, variables,
-                           connectionmatrix, tags_tsdata, dataset)
-
-    if transientplots:
-        diffplot, absplot = plot_transient_importances(variables,
-                                                       transientdict,
-                                                       basevaldict)
-        diffplot_filename = os.path.join(saveloc,
-                                         "{}_diffplot.pdf".format(scenario))
-        absplot_filename = os.path.join(saveloc,
-                                        "{}_absplot.pdf".format(scenario))
-        diffplot.savefig(diffplot_filename)
-        absplot.savefig(absplot_filename)
-
-    if importancegraph:
-        closedgraph, opengraph = \
-            create_importance_graph(variables, connectionmatrix,
-                                    openconnectionmatrix, gainmatrix,
-                                    rankingdict)
-        closedgraph_filename = os.path.join(saveloc,
-                                            "{}_closedgraph.gml".format(scenario))
-        opengraph_filename = os.path.join(saveloc,
-                                          "{}_opengraph.gml".format(scenario))
-
-        nx.readwrite.write_gml(closedgraph, closedgraph_filename)
-        nx.readwrite.write_gml(opengraph, opengraph_filename)
+    if singleranking:    
+        looprank_single(scenario, variables,
+                        connectionmatrix, gainmatrix)
+    
+    if transientranking:
+        [transientdict, basevaldict] = \
+            looprank_transient(scenario, sampling_rate, boxsize, boxnum, variables,
+                               connectionmatrix, tags_tsdata, dataset)
