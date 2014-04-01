f572b689e47bf60091daff15a18e81ed490eda2e
SimonStreicher <streichersj@gmail.com>
2014-04-01 13:26:40 +0200

    Made independent of specific references for test cases

--- demo_setup.py
+++ demo_setup.py
@@ -9,7 +9,7 @@ import json
 import os
 
 
-def runsetup(mode='test_cases', case='delaytests'):
+def runsetup(mode='test_cases', case='weightcalc_tests'):
     """Gets all required parameters from the case configuration file.
 
     Mode can be either 'test_cases' or 'plants' as required to get to the
@@ -17,16 +17,23 @@ def runsetup(mode='test_cases', case='delaytests'):
 
     """
 
-    # Load directories config file
-    dirs = json.load(open('config.json'))
-    # Get data and preferred export directories from directories config file
-    dataloc = os.path.expanduser(dirs['dataloc'])
-    saveloc = os.path.expanduser(dirs['saveloc'])
-    infodynamicsloc = os.path.expanduser(dirs['infodynamicsloc'])
+    if mode == 'test_cases':
+#        saveloc = os.path.join(os.path.dirname(__file__), 'texports')
+        saveloc = os.path.join('test_exports')
+#        casedir = os.path.join(os.path.dirname(__file__), 'tconfigs', case)
+        casedir = os.path.join('test_configs')
+        infodynamicsloc = 'infodynamics.jar'
 
-    # Define case data directory
-    casedir = os.path.join(dataloc, mode, case)
-    # Load case config file
-    caseconfig = json.load(open(os.path.join(casedir, case + '.json')))
+    elif mode == 'plants':
 
-    return saveloc, caseconfig, casedir, infodynamicsloc
+        # Load directories config file
+        dirs = json.load(open('config.json'))
+        # Get data and preferred export directories from
+        # directories config file
+        dataloc = os.path.expanduser(dirs['dataloc'])
+        saveloc = os.path.expanduser(dirs['saveloc'])
+        infodynamicsloc = os.path.expanduser(dirs['infodynamicsloc'])
+        # Define case data directory
+        casedir = os.path.join(dataloc, mode, case)
+
+    return saveloc, casedir, infodynamicsloc
