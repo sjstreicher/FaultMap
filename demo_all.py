d685d1ade4fe2399cd5f384b03381329c4d86664
SimonStreicher <streichersj@gmail.com>
2014-04-01 16:57:16 +0200

    Added tests to get full coverage analysis

--- demo_all.py
+++ demo_all.py
@@ -6,6 +6,12 @@ Created on Sun Mar 16 17:29:06 2014
 @author: Simon
 """
 
+# Demos
 execfile("demo_looprank.py")
 execfile("demo_weightcalc.py")
 execfile("demo_autoreg_tecalc.py")
+
+# Tests
+execfile("test_looprank.py")
+execfile("test_weightcalc.py")
+execfile("test_tecalc.py")
