788394f03c8c2bf03a5cd6b18698f04b7de81503
SimonStreicher <streichersj@gmail.com>
2014-04-01 14:18:03 +0200

    Renamed demo_estimate_delay.py to test_weightcalc.py

---
+++ test_weightcalc.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+
+Created on Thu Mar 13 09:44:16 2014
+
+@author: Simon
+"""
+
+from ranking.gaincalc import weightcalc
+
+if __name__ == '__main__':
+    mode = 'test_cases'
+    case = 'weightcalc_tests'
+    weightcalc(mode, case, False)
