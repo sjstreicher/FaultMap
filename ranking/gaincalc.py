6f89b9d2ac48abb398683a63bd9045a7150d3a4f
SimonStreicher <streichersj@gmail.com>
2014-04-01 14:17:16 +0200

    Added weightcalc function

--- ranking/gaincalc.py
+++ ranking/gaincalc.py
@@ -4,14 +4,20 @@
 
 """
 
+import os
 import csv
 import numpy as np
 import h5py
 import logging
+import jpype
+import json
+
+from datagen import *
 
 from sklearn import preprocessing
 from transentropy import calc_infodynamics_te as te_infodyns
 from transentropy import setup_infodynamics_te as te_setup
+from config_setup import runsetup
 
 
 def create_connectionmatrix(connection_loc):
@@ -264,7 +270,129 @@ def estimate_delay(variables, connectionmatrix, inputdata,
                 weight_array[affectedvarindex, causevarindex] = np.NAN
                 delay_array[affectedvarindex, causevarindex] = np.NAN
 
-#    if method == 'transfer_entropy':
-#        shutdownJVM()
-
     return weight_array, delay_array, datastore, data_header
+
+
+def writecsv_weightcalc(filename, items, header):
+    """CSV writer customized for use in weightcalc function."""
+
+    with open(filename, 'wb') as f:
+        csv.writer(f).writerow(header)
+        csv.writer(f).writerows(items)
+
+
+def weightcalc(mode, case, writeoutput=False):
+    """Reports the maximum partial correlation as well as associated delay
+    obtained by shifting the affected variable behind the causal variable a
+    specified set of delays.
+
+    Also reports whether the sign changed compared to no delay.
+
+    """
+
+    logging.basicConfig(level=logging.INFO)
+
+    saveloc, casedir, infodynamicsloc = runsetup(mode, case)
+
+    # Load case config file
+    caseconfig = json.load(open(os.path.join(casedir, case + '.json')))
+
+    # Get scenarios
+    scenarios = caseconfig['scenarios']
+    # Get sampling rate
+    sampling_rate = caseconfig['sampling_rate']
+    # Get data type
+    datatype = caseconfig['datatype']
+    # Get delay type
+    delaytype = caseconfig['delaytype']
+    # Get methods
+    methods = caseconfig['methods']
+    # Get size of sample vectors for tests
+    # Must be smaller than number of samples generated
+    testsize = caseconfig['testsize']
+    # Get number of delays to test
+    test_delays = caseconfig['test_delays']
+
+    if delaytype == 'datapoints':
+    # Include first n sampling intervals
+        delays = range(test_delays + 1)
+    elif delaytype == 'timevalues':
+    # Include first n 10-second shifts
+        delays = [val * (10.0/3600.0) for val in range(test_delays + 1)]
+
+    # Start JVM if required
+    if 'transfer_entropy' in methods:
+        # Change location of jar to match yours:
+        jarLocation = infodynamicsloc
+        # Start the JVM
+        # (add the "-Xmx" option with say 1024M if you get crashes
+        # due to not enough memory space)
+        jpype.startJVM(jpype.getDefaultJVMPath(), "-ea",
+                       "-Djava.class.path=" + jarLocation)
+
+    for scenario in scenarios:
+        logging.info("Running scenario {}".format(scenario))
+
+        # Get time series data
+        if datatype == 'file':
+            tags_tsdata = os.path.join(casedir, 'data',
+                                       caseconfig[scenario]['data'])
+            # Get connection (adjacency) matrix
+            connectionloc = os.path.join(casedir, 'connections',
+                                         caseconfig[scenario]['connections'])
+            # Get dataset name
+            dataset = caseconfig[scenario]['dataset']
+            # Get inputdata
+            inputdata = np.array(h5py.File(tags_tsdata, 'r')[dataset])
+            # Get the variables and connection matrix
+            [variables, connectionmatrix] = \
+                create_connectionmatrix(connectionloc)
+
+        elif datatype == 'function':
+            tags_tsdata_gen = caseconfig[scenario]['datagen']
+            connectionloc = caseconfig[scenario]['connections']
+            # TODO: Store function arguments in scenario config file
+            samples = caseconfig['gensamples']
+            delay = caseconfig['delay']
+            # Get inputdata
+            inputdata = eval(tags_tsdata_gen)(samples, delay)
+            # Get the variables and connection matrix
+            [variables, connectionmatrix] = eval(connectionloc)()
+
+        # Normalise (mean centre and variance scale) the input data
+        inputdata_norm = preprocessing.scale(inputdata, axis=0)
+
+        for method in methods:
+            logging.info("Method: " + method)
+
+            [weight_array, delay_array, datastore, data_header] = \
+                estimate_delay(variables, connectionmatrix, inputdata_norm,
+                               sampling_rate, testsize, delays, delaytype,
+                               method)
+
+            if writeoutput:
+                # Define export directories and filenames
+                datasavename = \
+                    os.path.join(saveloc, 'estimate_delay',
+                                 '{}_{}_{}_estimate_delay_data.csv'
+                                 .format(case, scenario, method))
+                value_array_savename = \
+                    os.path.join(saveloc, 'estimate_delay',
+                                 '{}_{}_{}_maxweight_array.csv'
+                                 .format(case, scenario, method))
+                delay_array_savename = \
+                    os.path.join(saveloc, 'estimate_delay',
+                                 '{}_{}_{}_delay_array.csv'
+                                 .format(case, scenario, method))
+
+                # Write arrays to file
+                np.savetxt(value_array_savename, weight_array, delimiter=',')
+                np.savetxt(delay_array_savename, delay_array, delimiter=',')
+
+                # Write datastore to file
+                writecsv_weightcalc(datasavename, datastore, data_header)
+
+    # Stop JVM if required
+    if 'transfer_entropy' in methods:
+        # Shutdown the JVM
+        jpype.shutdownJVM()
