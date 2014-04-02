54de92a9c48384e0fb5bc45496770101fb8c5994
Simon Streicher <streichersj@gmail.com>
2014-04-02 16:03:52 +0200

    Added a number of new test networks

--- networkgen.py
+++ networkgen.py
@@ -10,8 +10,8 @@ import numpy as np
 import json
 import os
 
-#filesloc = json.load(open('config.json'))
-#saveloc = os.path.expanduser(filesloc['saveloc'])
+filesloc = json.load(open('config.json'))
+saveloc = os.path.expanduser(filesloc['saveloc'])
 
 
 def fullconn_equal():
@@ -183,5 +183,158 @@ def series_incomingon2ndand4th():
 #    nx.write_gml(testgraph, os.path.join(saveloc, 'testgraphs',
 #                                         "series_incomingon2ndand4th.gml"))
 #    nx.draw(testgraph)
+
+    return connections, gainmatrix, variables, testgraph
+
+
+def series_disjoint_equal():
+    """Creates two sets of three tags connected in series with unit weights on
+    all edges.
+
+    """
+
+    variables = ['X 1', 'X 2', 'X 3', 'Y 1', 'Y 2', 'Y 3']
+
+    connections = np.array([[0, 0, 0, 0, 0, 0],
+                            [1, 0, 0, 0, 0, 0],
+                            [0, 1, 0, 0, 0, 0],
+                            [0, 0, 0, 0, 0, 0],
+                            [0, 0, 0, 1, 0, 0],
+                            [0, 0, 0, 0, 1, 0]])
+
+    gainmatrix = np.array([[0, 0, 0, 0, 0, 0],
+                           [1, 0, 0, 0, 0, 0],
+                           [0, 1, 0, 0, 0, 0],
+                           [0, 0, 0, 0, 0, 0],
+                           [0, 0, 0, 1, 0, 0],
+                           [0, 0, 0, 0, 1, 0]])
+
+    testgraph = buildgraph(variables, gainmatrix, connections)
+
+#    nx.write_gml(testgraph, os.path.join(saveloc, 'testgraphs',
+#                                         "series_disjoint_equal.gml"))
+#    nx.draw(testgraph)
+
+    return connections, gainmatrix, variables, testgraph
+
+
+def series_disjoint_unequal():
+    """Creates two sets of three tags connected in series with unit weights on
+    the edges of one series and weights of 2 on that of the other.
+
+    """
+
+    variables = ['X 1', 'X 2', 'X 3', 'Y 1', 'Y 2', 'Y 3']
+
+    connections = np.array([[0, 0, 0, 0, 0, 0],
+                            [1, 0, 0, 0, 0, 0],
+                            [0, 1, 0, 0, 0, 0],
+                            [0, 0, 0, 0, 0, 0],
+                            [0, 0, 0, 1, 0, 0],
+                            [0, 0, 0, 0, 1, 0]])
+
+    gainmatrix = np.array([[0, 0, 0, 0, 0, 0],
+                           [1, 0, 0, 0, 0, 0],
+                           [0, 1, 0, 0, 0, 0],
+                           [0, 0, 0, 0, 0, 0],
+                           [0, 0, 0, 2, 0, 0],
+                           [0, 0, 0, 0, 2, 0]])
+
+    testgraph = buildgraph(variables, gainmatrix, connections)
+
+#    nx.write_gml(testgraph, os.path.join(saveloc, 'testgraphs',
+#                                         "series_disjoint_equal.gml"))
+#    nx.draw(testgraph)
+
+    return connections, gainmatrix, variables, testgraph
+
+
+def series_disjoint_unequalsource():
+    """Creates two sets of three tags connected in series with unit weights on
+    the edges of one series and weights of 2 on that of the other.
+
+    """
+
+    variables = ['X 1', 'X 2', 'X 3', 'Y 1', 'Y 2', 'Y 3', 'I 1']
+
+    connections = np.array([[0, 0, 0, 0, 0, 0, 1],
+                            [1, 0, 0, 0, 0, 0, 0],
+                            [0, 1, 0, 0, 0, 0, 0],
+                            [0, 0, 0, 0, 0, 0, 1],
+                            [0, 0, 0, 1, 0, 0, 0],
+                            [0, 0, 0, 0, 1, 0, 0],
+                            [0, 0, 0, 0, 0, 0, 0]])
+
+    gainmatrix = np.array([[0, 0, 0, 0, 0, 0, 1],
+                           [1, 0, 0, 0, 0, 0, 0],
+                           [0, 1, 0, 0, 0, 0, 0],
+                           [0, 0, 0, 0, 0, 0, 2],
+                           [0, 0, 0, 1, 0, 0, 0],
+                           [0, 0, 0, 0, 1, 0, 0],
+                           [0, 0, 0, 0, 0, 0, 0]])
+
+    testgraph = buildgraph(variables, gainmatrix, connections)
+
+#    nx.write_gml(testgraph, os.path.join(saveloc, 'testgraphs',
+#                                         "series_disjoint_equal.gml"))
+#    nx.draw(testgraph)
+
+    return connections, gainmatrix, variables, testgraph
+
+
+def circle_equal():
+    """Creates five tags connected in a circle with unit weights on
+    all edges.
+
+    """
+
+    variables = ['X 1', 'X 2', 'X 3', 'X 4', 'X 5']
+
+    connections = np.array([[0, 0, 0, 0, 1],
+                            [1, 0, 0, 0, 0],
+                            [0, 1, 0, 0, 0],
+                            [0, 0, 1, 0, 0],
+                            [0, 0, 0, 1, 0]])
+
+    gainmatrix = np.array([[0, 0, 0, 0, 1],
+                           [1, 0, 0, 0, 0],
+                           [0, 1, 0, 0, 0],
+                           [0, 0, 1, 0, 0],
+                           [0, 0, 0, 1, 0]])
+
+    testgraph = buildgraph(variables, gainmatrix, connections)
+
+    nx.write_gml(testgraph, os.path.join(saveloc, 'testgraphs',
+                                         "circle_equal.gml"))
+    nx.draw(testgraph)
+
+    return connections, gainmatrix, variables, testgraph
+
+
+def circle_unequalon2to3():
+    """Creates five tags connected in a circle with unit weights on
+    all edges except for one.
+
+    """
+
+    variables = ['X 1', 'X 2', 'X 3', 'X 4', 'X 5']
+
+    connections = np.array([[0, 0, 0, 0, 1],
+                            [1, 0, 0, 0, 0],
+                            [0, 1, 0, 0, 0],
+                            [0, 0, 1, 0, 0],
+                            [0, 0, 0, 1, 0]])
+
+    gainmatrix = np.array([[0, 0, 0, 0, 1],
+                           [1, 0, 0, 0, 0],
+                           [0, 2, 0, 0, 0],
+                           [0, 0, 1, 0, 0],
+                           [0, 0, 0, 1, 0]])
+
+    testgraph = buildgraph(variables, gainmatrix, connections)
+
+    nx.write_gml(testgraph, os.path.join(saveloc, 'testgraphs',
+                                         "circle_unequalon2to3.gml"))
+    nx.draw(testgraph)
 
     return connections, gainmatrix, variables, testgraph
