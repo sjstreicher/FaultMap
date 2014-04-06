1cf2498378f7568cde5fb42d1c9221733d8428c9
Simon Streicher <streichersj@gmail.com>
2014-04-06 22:45:19 +0200

    Current version used

--- csv_to_hdf5.py
+++ csv_to_hdf5.py
@@ -8,19 +8,30 @@ Created on Tue Mar 11 10:29:33 2014
 import numpy as np
 import tables as tb
 import json
+from config_setup import runsetup
+import os
+
+mode = 'plants'
+case = 'epu5_compressor'
+
+saveloc, casedir, _ = runsetup(mode, case)
+caseconfig = json.load(open(os.path.join(casedir, case + '.json')))
+scenarios = caseconfig['scenarios']
+
+scenario = scenarios[0]
+
+tags_tsdata = os.path.join(casedir, 'data',
+                           caseconfig[scenario]['data'])
+dataset = caseconfig[scenario]['dataset']
 
 # Define the datafile name as defined in the config file
 # This is also used to name the created file name and filename
-datafile = 'data_pressetstep_mod'
+#datafile = 'data_raw'
 # Define any connection file with the same variables as the data file columns
 # Only needed when creating columns
 #connectionfile = 'all_connections_measonly'
 
-filesloc = json.load(open('config.json'))
-# Load the csv file
-tags_tsdata = filesloc[datafile + "_csv"]
-# Defines the destination of the created hdf5 file
-saveloc = filesloc['savelocation']
+
 #connection_loc = filesloc[connectionfile]
 
 # Create a description instance
@@ -31,14 +42,14 @@ saveloc = filesloc['savelocation']
 #value = ['float64'] * len(keys)
 #data_description = np.dtype(zip(keys, value))
 
-hdf5writer = tb.open_file(saveloc + datafile + '.h5', 'w')
+hdf5writer = tb.open_file(saveloc + dataset + '.h5', 'w')
 data = np.genfromtxt(tags_tsdata, delimiter=',')
 
 #table = hdf5writer.create_table(hdf5writer.root, description=data_description,
 #                                name=datafile,
 #                                title=datafile)
                                 
-array = hdf5writer.create_array(hdf5writer.root, datafile, data)
+array = hdf5writer.create_array(hdf5writer.root, dataset, data)
             
 #table.append(data)
 #table.flush()
