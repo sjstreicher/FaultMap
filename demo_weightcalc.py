f202cf4106c0f59a77c487e61b96b16758038935
Simon Streicher <streichersj@gmail.com>
2014-04-06 22:46:09 +0200

    Added epu5_compressor case

--- demo_weightcalc.py
+++ demo_weightcalc.py
@@ -11,10 +11,15 @@ logging.basicConfig(level=logging.INFO)
 writeoutput = True
 
 
-mode = 'test_cases'
-case = 'weightcalc_tests'
-weightcalc(mode, case, writeoutput)
+#mode = 'test_cases'
+#case = 'weightcalc_tests'
+#weightcalc(mode, case, writeoutput)
+
+#mode = 'plants'
+#case = 'tennessee_eastman'
+#weightcalc(mode, case, writeoutput)
 
 mode = 'plants'
-case = 'tennessee_eastman'
+case = 'epu5_compressor'
 weightcalc(mode, case, writeoutput)
+
