a6a05093e02fe9c8c717acb3164a26ee20a8d9c0
Simon Streicher <streichersj@gmail.com>
2014-04-16 11:53:17 +0200

    Parametrised runs for selective execution

--- demo_weightcalc.py
+++ demo_weightcalc.py
@@ -10,15 +10,19 @@ logging.basicConfig(level=logging.INFO)
 
 writeoutput = True
 
+runs = ['epu5_compressor']
 
-#mode = 'test_cases'
-#case = 'weightcalc_tests'
-#weightcalc(mode, case, writeoutput)
+if 'weightcalc_tests' in runs:
+    mode = 'test_cases'
+    case = 'weightcalc_tests'
+    weightcalc(mode, case, writeoutput)
 
-#mode = 'plants'
-#case = 'tennessee_eastman'
-#weightcalc(mode, case, writeoutput)
+if 'tennessee_eastman' in runs:
+    mode = 'plants'
+    case = 'tennessee_eastman'
+    weightcalc(mode, case, writeoutput)
 
-mode = 'plants'
-case = 'epu5_compressor'
-weightcalc(mode, case, writeoutput)
+if 'epu5_compressor' in runs:
+    mode = 'plants'
+    case = 'epu5_compressor'
+    weightcalc(mode, case, writeoutput)
