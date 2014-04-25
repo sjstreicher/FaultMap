305682ec34879467b4cbd8fb07e7b12cb536197e
Simon Streicher <streichersj@gmail.com>
2014-04-25 16:32:39 +0200

    Parametrised running of cases

--- demo_looprank.py
+++ demo_looprank.py
@@ -9,22 +9,28 @@ logging.basicConfig(level=logging.INFO)
 
 writeoutput = True
 
-#mode = 'test_cases'
-#case = 'noderank_tests'
-#
-#for dummycreation in [False, True]:
-#    looprank_static(mode, case, dummycreation, writeoutput)
-#
-#mode = 'plants'
-#case = 'tennessee_eastman'
-#
-#for dummycreation in [False, True]:
-#    looprank_static(mode, case, dummycreation, writeoutput)
-#    looprank_transient(mode, case, dummycreation, writeoutput, True)
+runs = ['u238_alcoholrecovery']
 
+if 'noderank_tests' in runs:
+    mode = 'test_cases'
+    case = 'noderank_tests'
+    for dummycreation in [False, True]:
+        looprank_static(mode, case, dummycreation, writeoutput)
 
-mode = 'plants'
-case = 'epu5_compressor'
+if 'tennessee_eastman' in runs:
+    mode = 'plants'
+    case = 'tennessee_eastman'
+    for dummycreation in [False, True]:
+        looprank_static(mode, case, dummycreation, writeoutput)
 
-for dummycreation in [False, True]:
-    looprank_static(mode, case, dummycreation, writeoutput)
+if 'epu5_compressor' in runs:
+    mode = 'plants'
+    case = 'epu5_compressor'
+    for dummycreation in [False, True]:
+        looprank_static(mode, case, dummycreation, writeoutput)
+
+if 'u238_alcoholrecovery' in runs:
+    mode = 'plants'
+    case = 'u238_alcoholrecovery'
+    for dummycreation in [False, True]:
+        looprank_static(mode, case, dummycreation, writeoutput)
