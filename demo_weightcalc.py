5e7204d237f3928c1c52d76f7c67e77b078d8f51
Simon Streicher <streichersj@gmail.com>
2014-05-01 19:17:08 +0200

    Changed the way demos is run

--- demo_weightcalc.py
+++ demo_weightcalc.py
@@ -9,24 +9,8 @@ logging.basicConfig(level=logging.INFO)
 
 writeoutput = True
 
-runs = ['u238_alcoholrecovery']
+mode = 'plants'
+cases = ['tennessee_eastman']
 
-if 'weightcalc_tests' in runs:
-    mode = 'test_cases'
-    case = 'weightcalc_tests'
-    weightcalc(mode, case, writeoutput)
-
-if 'tennessee_eastman' in runs:
-    mode = 'plants'
-    case = 'tennessee_eastman'
-    weightcalc(mode, case, writeoutput)
-
-if 'epu5_compressor' in runs:
-    mode = 'plants'
-    case = 'epu5_compressor'
-    weightcalc(mode, case, writeoutput)
-
-if 'u238_alcoholrecovery' in runs:
-    mode = 'plants'
-    case = 'u238_alcoholrecovery'
+for case in cases:
     weightcalc(mode, case, writeoutput)
