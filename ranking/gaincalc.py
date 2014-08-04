0aafd660a44c7f1f260e057a941e927353164d26
Simon Streicher <streichersj@gmail.com>
2014-08-04 21:49:35 +0200

    Plot bandgap filtered FFTs

--- ranking/gaincalc.py
+++ ranking/gaincalc.py
@@ -198,7 +198,7 @@ class WeightcalcData:
         # FFT the data and write back in format that can be analysed in
         # TOPCAT in a plane plot
         data_processing.fft_calculation(raw_tsdata,
-                                        self.inputdata_normalised,
+                                        self.inputdata_originalrate,
                                         self.variables,
                                         self.sampling_rate,
                                         self.sampling_unit,
