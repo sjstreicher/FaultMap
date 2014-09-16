f7b0bbae9687045cbcae86e0b9c6f8f347841289
SimonStreicher <streichersj@gmail.com>
2014-09-16 20:26:01 +0200

    Added transfer entropy vs sampling rate graph

--- generate_figures.py
+++ generate_figures.py
@@ -9,6 +9,7 @@ the LaTeX repository.
 import os
 import logging
 import matplotlib.pyplot as plt
+import numpy as np
 
 import config_setup
 from ranking import data_processing
@@ -23,7 +24,7 @@ graph_filename_template = os.path.join(graphs_savedir, '{}.pdf')
 # Some settings appropriate to the next couple of figures
 
 
-def filename(name):
+def filename():
     return filename_template.format(case, scenario, method, name)
 
 
@@ -44,7 +45,7 @@ plt.rc('font', family='serif')
 # Figure to show dependence of absolute value of transfer entropy on time
 # constant of the process
 
-sourcefile = filename(name)
+sourcefile = filename()
 
 
 # Load CSV file in order to create graphs
@@ -70,8 +71,8 @@ if not os.path.exists(testlocation):
     plt.plot(valuematrix[:, 0], valuematrix[:, 5], marker="o", markersize=4,
              label=r'$\tau = 5.0$ seconds')
 
-    plt.ylabel(r'Absolute transfer entropy (bits)', fontsize=12)
-    plt.xlabel(r'Delay (samples)', fontsize=12)
+    plt.ylabel(r'Absolute transfer entropy (bits)', fontsize=14)
+    plt.xlabel(r'Delay (samples)', fontsize=14)
     plt.legend()
 
     plt.axis([70, 130, -0.05, 0.20])
@@ -86,7 +87,7 @@ else:
 # Now for directional transfer entropy
 method = 'directional_transfer_entropy'
 
-sourcefile = filename(name)
+sourcefile = filename()
 valuematrix, headers = data_processing.read_header_values_datafile(sourcefile)
 
 graphname = 'firstorder_noiseonly_dir_scen01'
@@ -106,8 +107,8 @@ if not os.path.exists(testlocation):
     plt.plot(valuematrix[:, 0], valuematrix[:, 5], marker="o", markersize=4,
              label=r'$\tau = 5.0$ seconds')
 
-    plt.ylabel(r'Directional transfer entropy (bits)', fontsize=12)
-    plt.xlabel(r'Delay (samples)', fontsize=12)
+    plt.ylabel(r'Directional transfer entropy (bits)', fontsize=14)
+    plt.xlabel(r'Delay (samples)', fontsize=14)
     plt.legend()
 
     plt.axis([70, 130, -0.10, 0.15])
@@ -122,12 +123,153 @@ else:
 
 method = 'directional_transfer_entropy'
 
-sourcefile = filename(name)
-valuematrix_dir, headers = data_processing.read_header_values_datafile(sourcefile)
+sourcefile = filename()
+valuematrix_dir, headers = \
+    data_processing.read_header_values_datafile(sourcefile)
 
 method = 'absolute_transfer_entropy'
 
-sourcefile = filename(name)
-valuematrix_abs, headers = data_processing.read_header_values_datafile(sourcefile)
+sourcefile = filename()
+valuematrix_abs, headers = \
+    data_processing.read_header_values_datafile(sourcefile)
+
+max_directional_te = valuematrix_dir[100][1:]
+max_absolute_te = valuematrix_abs[100][1:]
+
+taus = [0.2, 0.5, 1.0, 2.0, 5.0]
+
+#dir_vals = [te / tau for te, tau in zip(max_directional_te, taus)]
+#abs_vals = [te / tau for te, tau in zip(max_absolute_te, taus)]
+
+dir_vals = [te for te in max_directional_te]
+abs_vals = [te for te in max_absolute_te]
+
+graphname = 'firstorder_noiseonly_te_vs_tau_logplot_scen01'
+
+directional_params = np.polyfit(np.log(taus), np.log(max_directional_te), 1)
+
+absolute_params = np.polyfit(np.log(taus), np.log(max_absolute_te), 1)
+
+dir_fit_y = [(i*directional_params[0] + directional_params[1])
+             for i in np.log(taus)]
+abs_fit_y = [(i*absolute_params[0] + absolute_params[1])
+             for i in np.log(taus)]
+
+dir_fitted_vals = [np.exp(te) for te in dir_fit_y]
+abs_fitted_vals = [np.exp(te) for te in abs_fit_y]
+#dir_fitted_vals = [np.exp(te) / tau for te, tau in zip(dir_fit_y, taus)]
+#abs_fitted_vals = [np.exp(te) / tau for te, tau in zip(abs_fit_y, taus)]
+
+# Test whether the figure already exists
+testlocation = graph_filename(graphname)
+if not os.path.exists(testlocation):
+    plt.figure(1, (12, 6))
+    plt.loglog(taus, abs_vals, ".", marker="o", markersize=4,
+               label=r'absolute')
+    plt.loglog(taus, dir_vals, ".", marker="o", markersize=4,
+               label=r'directional')
+    plt.loglog(taus, abs_fitted_vals, "--",
+               label=r'absolute fit')
+    plt.loglog(taus, dir_fitted_vals, "--",
+               label=r'directional fit')
+
+    plt.ylabel(r'Transfer entropy (bits)', fontsize=14)
+    plt.xlabel(r'Time constant ($\tau$)', fontsize=14)
+    plt.legend()
+
+    plt.savefig(graph_filename(graphname))
+    plt.close()
+
+else:
+    logging.info("The requested graph has already been drawn")
+
+# Investigate effect of noise sampling rate / simulation time step on
+# values of transfer entropies
+
+# Approach: Take the maximum transfer entropy for a time constant of unity
+# from sets 1, 2, and 3 with noise sampling intervals of
+# [0.1, 0.01, 1.0] respectively.
+
+graphname = 'firstorder_noiseonly_sampling_rate_effect'
+
+# Get the different data vectors required
+# Set 1 data
+method = 'absolute_transfer_entropy'
+scenario = 'noiseonly_nosubs_set1'
+sourcefile = filename()
+valuematrix_set1, _ = \
+    data_processing.read_header_values_datafile(sourcefile)
+
+scenario = 'noiseonly_nosubs_set2'
+sourcefile = filename()
+valuematrix_set2, _ = \
+    data_processing.read_header_values_datafile(sourcefile)
+
+scenario = 'noiseonly_nosubs_set3'
+sourcefile = filename()
+valuematrix_set3, _ = \
+    data_processing.read_header_values_datafile(sourcefile)
+
+method = 'directional_transfer_entropy'
+scenario = 'noiseonly_nosubs_set1'
+sourcefile = filename()
+valuematrix_set1_dir, _ = \
+    data_processing.read_header_values_datafile(sourcefile)
+
+scenario = 'noiseonly_nosubs_set2'
+sourcefile = filename()
+valuematrix_set2_dir, _ = \
+    data_processing.read_header_values_datafile(sourcefile)
+
+scenario = 'noiseonly_nosubs_set3'
+sourcefile = filename()
+valuematrix_set3_dir, _ = \
+    data_processing.read_header_values_datafile(sourcefile)
 
 
+# The maximum values associated with a time constant of unity is in the
+# [100][3] entry
+
+te_abs_vals = [valuematrix_set2[100][3], valuematrix_set1[100][3],
+               valuematrix_set3[100][3]]
+te_dir_vals = [valuematrix_set2_dir[100][3], valuematrix_set1_dir[100][3],
+               valuematrix_set3_dir[100][3]]
+
+sampling_intervals = [0.01, 0.1, 1.0]
+
+directional_params = np.polyfit(np.log(sampling_intervals),
+                                np.log(te_dir_vals), 1)
+
+absolute_params = np.polyfit(np.log(sampling_intervals),
+                                np.log(te_abs_vals), 1)
+
+dir_fit_y = [(i*directional_params[0] + directional_params[1])
+             for i in np.log(sampling_intervals)]
+abs_fit_y = [(i*absolute_params[0] + absolute_params[1])
+             for i in np.log(sampling_intervals)]
+
+dir_fitted_vals = [np.exp(te) for te in dir_fit_y]
+abs_fitted_vals = [np.exp(te) for te in abs_fit_y]
+
+# Test whether the figure already exists
+testlocation = graph_filename(graphname)
+#if not os.path.exists(testlocation):
+if not False:
+    plt.figure(1, (12, 6))
+    plt.loglog(sampling_intervals, te_abs_vals, ".", marker="o", markersize=4,
+               label=r'absolute')
+    plt.loglog(sampling_intervals, te_dir_vals, ".", marker="o", markersize=4,
+               label=r'directional')
+    plt.loglog(sampling_intervals, abs_fitted_vals, "--", markersize=4,
+               label=r'absolute fit')
+    plt.loglog(sampling_intervals, dir_fitted_vals, "--", markersize=4,
+               label=r'directional fit')
+    plt.ylabel(r'Transfer entropy (bits)', fontsize=14)
+    plt.xlabel(r'Noise sampling rate ($\frac{1}{s}$)', fontsize=14)
+    plt.legend()
+
+    plt.savefig(graph_filename(graphname))
+    plt.close()
+
+else:
+    logging.info("The requested graph has already been drawn")
