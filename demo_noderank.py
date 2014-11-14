6076fbf460b1e7f1883a2be5c48fd3d7b9a6ac7c
Simon Streicher <streichersj@gmail.com>
2014-11-14 11:27:12 +0200

    Added demo_noderank.py Which I forgot to track earlier

---
+++ demo_noderank.py
@@ -0,0 +1,25 @@
+"""
+@author: Simon Streicher
+
+"""
+
+import logging
+import json
+import os
+
+import config_setup
+from ranking.noderank import noderankcalc
+
+logging.basicConfig(level=logging.INFO)
+
+dataloc, _ = config_setup.get_locations()
+noderank_config = json.load(open(os.path.join(dataloc, 'config'
+                                              '_noderank' + '.json')))
+
+writeoutput = noderank_config['writeoutput']
+#writeoutput = False
+mode = noderank_config['mode']
+cases = noderank_config['cases']
+
+for case in cases:
+    noderankcalc(mode, case, writeoutput)
