3ad19b6d2276c23e9d8c62aca9010e96006b900a
Simon Streicher <streichersj@gmail.com>
2015-02-15 13:25:00 +0200

    Renamed some "demo_" scripts to "run_"

---
+++ run_graphreduce.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+"""
+@author: Simon Streicher
+
+"""
+import logging
+import json
+import os
+
+import config_setup
+from ranking.graphreduce import reducegraph
+
+logging.basicConfig(level=logging.INFO)
+
+dataloc, _ = config_setup.get_locations()
+graphreduce_config = json.load(open(os.path.join(dataloc, 'config'
+                                              '_graphreduce' + '.json')))
+
+writeoutput = False
+#writeoutput = graphreduce_config['writeoutput']
+mode = graphreduce_config['mode']
+cases = graphreduce_config['cases']
+
+for case in cases:
+    reducegraph(mode, case, writeoutput)
