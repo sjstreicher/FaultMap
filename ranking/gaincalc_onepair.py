ed42b46b813389633ad0a2f3141b57020c0abdb0
Simon Streicher <streichersj@gmail.com>
2015-03-16 13:48:40 +0200

    Removed duplicate writecsv_weightcalc function

--- ranking/gaincalc_onepair.py
+++ ranking/gaincalc_onepair.py
@@ -13,7 +13,7 @@ from functools import partial
 import pathos
 from pathos.multiprocessing import ProcessingPool as Pool
 
-do_multiprocessing = False
+do_multiprocessing = True
 
 
 def writecsv_weightcalc(filename, datalines_basis, new_dataline, header):
