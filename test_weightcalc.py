95b97393a17ed03d9761fef1923e9a6baf478007
Simon Streicher <streichersj@gmail.com>
2015-04-14 13:49:41 +0200

    Implemented single and multi-processing tests

--- test_weightcalc.py
+++ test_weightcalc.py
@@ -15,8 +15,11 @@ class TestWeightcalc(unittest.TestCase):
         self.mode = 'tests'
         self.case = 'weightcalc_tests'
 
-    def test_weightcalc(self):
-        weightcalc(self.mode, self.case)
+    def test_weightcalc_singleprocess(self):
+        weightcalc(self.mode, self.case, False, False, False, False)
+
+    def test_weightcalc_multiprocess(self):
+        weightcalc(self.mode, self.case, False, False, False, True)
 
 if __name__ == '__main__':
     multiprocessing.freeze_support()
