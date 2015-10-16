7882480111ec179e4f24187a9f7b2c03434892be
Simon Streicher <streichersj@gmail.com>
2015-10-16 09:57:03 +0200

    Implemented a Kraskov estimator test case as well

--- test_autoreg_tecalc.py
+++ test_autoreg_tecalc.py
@@ -2,8 +2,6 @@
 """Verifies the working of the transfer entropy calculation code by means of
 an example on autoregressive data with known time delay.
 
-Created on Mon Feb 24 14:56:25 2014
-
 @author: Simon Streicher
 """
 
@@ -39,7 +37,8 @@ class TestAutoregressiveTransferEntropy(unittest.TestCase):
             jpype.startJVM(jpype.getDefaultJVMPath(), "-ea",
                            "-Djava.class.path=" + infodynamicsloc)
 
-        self.entropies_infodyn = []
+    def test_peakentropy_infodyn_kernel(self):
+        self.entropies_infodyn_kernel = []
         for timelag in range(self.delay-5, self.delay+6):
             print "Results for timelag of: ", str(timelag)
             [x_pred, x_hist, y_hist] = autoreg_datagen(self.delay, timelag,
@@ -56,18 +55,51 @@ class TestAutoregressiveTransferEntropy(unittest.TestCase):
 
             # Calculate transfer entropy according to infodynamics method:
 
-            result_infodyn, _ = te_info('infodynamics.jar',
-                                        True, 'kernel',
-                                        x_hist_norm[0], y_hist_norm[0])
-            self.entropies_infodyn.append(result_infodyn)
+            result_infodyn, _, _ = te_info('infodynamics.jar',
+                                           True, 'kernel',
+                                           x_hist_norm[0], y_hist_norm[0])
+            self.entropies_infodyn_kernel.append(result_infodyn)
             print("Infodynamics TE result: %.4f bits" % (result_infodyn))
 
-        print self.entropies_infodyn
+        print self.entropies_infodyn_kernel
 
-    def test_peakentropy_infodyn(self):
-        maxval = max(self.entropies_infodyn)
+        maxval = max(self.entropies_infodyn_kernel)
         # The maximum lags with one sample
-        delayedval = self.entropies_infodyn[self.delay-1]
+        delayedval = self.entropies_infodyn_kernel[self.delay-1]
+        self.assertEqual(maxval, delayedval)
+
+    def test_peakentropy_infodyn_kraskov(self):
+        self.entropies_infodyn_kraskov = []
+        for timelag in range(self.delay-5, self.delay+6):
+            print "Results for timelag of: ", str(timelag)
+            [x_pred, x_hist, y_hist] = autoreg_datagen(self.delay, timelag,
+                                                       self.samples,
+                                                       self.sub_samples)
+
+            # Normalize data
+            # Not explicitly required as this is done by infodyns package if
+            # setProperty("NORMALISE", "true" is called), but good practice
+            # for general example.
+
+            x_hist_norm = preprocessing.scale(x_hist, axis=1)
+            y_hist_norm = preprocessing.scale(y_hist, axis=1)
+
+            # Calculate transfer entropy according to infodynamics method:
+
+            result_infodyn, _, properties = \
+                te_info('infodynamics.jar', True, 'kraskov',
+                        x_hist_norm[0], y_hist_norm[0],
+                        auto_embed=True)
+            self.entropies_infodyn_kraskov.append(result_infodyn)
+            print("Infodynamics TE result: %.4f nats" % (result_infodyn))
+
+            print properties
+
+        print self.entropies_infodyn_kraskov
+
+        maxval = max(self.entropies_infodyn_kraskov)
+        # The maximum lags with one sample
+        delayedval = self.entropies_infodyn_kraskov[self.delay-1]
         self.assertEqual(maxval, delayedval)
 
 if __name__ == '__main__':
