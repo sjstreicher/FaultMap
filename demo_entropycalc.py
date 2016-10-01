541d33d0974b68661a1f6bfbd4502f085e60025d
Simon Streicher <streichersj@gmail.com>
2016-10-01 18:00:50 +0200

    Changes nats to bits Also cleans up a bit

--- demo_entropycalc.py
+++ demo_entropycalc.py
@@ -15,12 +15,12 @@ estimator = 'kernel'
 
 
 #class SpoofClass(object):
-#    
+#
 #    def __init__(self, infodynamicsloc, normalise):
 #        self.infodynamicsloc = infodynamicsloc
 #        self.normalise = normalise
-#        
-#weightcalcdata = SpoofClass(infodynamics_loc, True) 
+#
+#weightcalcdata = SpoofClass(infodynamics_loc, True)
 
 
 sns.set_style("darkgrid")
@@ -50,14 +50,14 @@ causevarindex = 0
 affectedvarindex = 1
 
 def gaussian_entropy(data):
-    "Returns entropy of Gaussian signal in nats"
+    "Returns entropy of Gaussian signal in bits"
     variance = np.var(data)
     entropy = 0.5 * np.log2(2.0 * np.pi * np.e * variance)
     return entropy
 
 def silverman_bandwidth(vardata):
     return (1.06 * np.std(vardata) * (len(vardata) ** (-1/5)))
-        
+
 def calculate_entropies(vardata, estimator=estimator):
 #    kernel_bandwidth = silverman_bandwidth(vardata)
     kernel_bandwidth = 0.8
@@ -69,9 +69,9 @@ def calculate_entropies(vardata, estimator=estimator):
     signalent_kernel = transentropy.calc_infodynamics_entropy(
         entropyCalc, vardata, estimator)
     signalent_gaussian = gaussian_entropy(vardata)
-    
+
     return signalent_kernel, signalent_gaussian
-    
+
 #corr_boxresults = []
 #te_boxresults = []
 signalent_cause_kernel_boxresults = []
@@ -82,13 +82,13 @@ for boxindex, box in enumerate(boxes):
     print "Now processing box: " + str(boxindex + 1)
     vardata_cause = box.T[0]
     vardata_effect = box.T[1]
-    
+
     signalent_cause_kernel, signalent_cause_gaussian = \
         calculate_entropies(vardata_cause)
-    
+
     signalent_affected_kernel, signalent_affected_gaussian = \
         calculate_entropies(vardata_effect)
-    
+
     signalent_cause_kernel_boxresults.append(signalent_cause_kernel)
     signalent_cause_gaussian_boxresults.append(signalent_cause_gaussian)
     signalent_affected_kernel_boxresults.append(signalent_affected_kernel)
