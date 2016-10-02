bb911751d3e5a66148e49314fc215b754329bd35
Simon Streicher <streichersj@gmail.com>
2016-10-02 22:39:04 +0200

    Makes tests faster

--- test_all.py
+++ test_all.py
@@ -9,46 +9,62 @@ import unittest
 from ranking.gaincalc import weightcalc
 from ranking.data_processing import result_reconstruction
 from ranking.data_processing import trend_extraction
-#from ranking.noderank import noderankcalc
-#from ranking.graphreduce import reducegraph
+from ranking.noderank import noderankcalc
+from ranking.graphreduce import reducegraph
+from plotting.plotter import plotdraw
 
 
-class TestWeightCalculation(unittest.TestCase):
-
-    def setUp(self):
-        self.mode = 'tests'
-        self.case = 'fulldemo'
-
-    def test_weightcalc_singleprocess(self):
-        weightcalc(self.mode, self.case, True, True, True, False)
-
-    def test_weightcalc_multiprocess(self):
-        weightcalc(self.mode, self.case, False, False, False, True)
-
-
-class TestCreateArrays(unittest.TestCase):
+class TestFullDemo(unittest.TestCase):
 
     def setUp(self):
         self.mode = 'tests'
-        self.case = 'fulldemo'
-        weightcalc(self.mode, self.case, True, False, False, True)
+        self.case = 'quickdemo'
 
-    def test_createarrays(self):
+    def test_full_analysis(self):
+        weightcalc(self.mode, self.case, True, False, False, False)
         result_reconstruction(self.mode, self.case, True)
-
-
-class TestTrendExtraction(unittest.TestCase):
-
-    def setUp(self):
-        self.mode = 'tests'
-        self.case = 'fulldemo'
-        weightcalc(self.mode, self.case, True, False, False, True)
-        result_reconstruction(self.mode, self.case, True)
-
-    def test_trendextraction(self):
         trend_extraction(self.mode, self.case, True)
+        noderankcalc(self.mode, self.case, True)
+        reducegraph(self.mode, self.case, True)
+        plotdraw(self.mode, self.case, True)
 
 
+#class TestWeightCalculation(unittest.TestCase):
+#
+#    def setUp(self):
+#        self.mode = 'tests'
+#        self.case = 'fulldemo'
+#
+#    def test_weightcalc_singleprocess(self):
+#        weightcalc(self.mode, self.case, True, True, True, False)
+#
+#    def test_weightcalc_multiprocess(self):
+#        weightcalc(self.mode, self.case, False, False, False, True)
+#
+#
+#class TestCreateArrays(unittest.TestCase):
+#
+#    def setUp(self):
+#        self.mode = 'tests'
+#        self.case = 'fulldemo'
+#        weightcalc(self.mode, self.case, True, False, False, True)
+#
+#    def test_createarrays(self):
+#        result_reconstruction(self.mode, self.case, True)
+#
+#
+#class TestTrendExtraction(unittest.TestCase):
+#
+#    def setUp(self):
+#        self.mode = 'tests'
+#        self.case = 'fulldemo'
+#        weightcalc(self.mode, self.case, True, False, False, True)
+#        result_reconstruction(self.mode, self.case, True)
+#
+#    def test_trendextraction(self):
+#        trend_extraction(self.mode, self.case, True)
+#
+#
 #class TestNodeRanking(unittest.TestCase):
 #
 #    def setUp(self):
@@ -70,6 +86,7 @@ class TestTrendExtraction(unittest.TestCase):
 #        weightcalc(self.mode, self.case, True, False, False, True)
 #        result_reconstruction(self.mode, self.case, True)
 #        trend_extraction(self.mode, self.case, True)
+#        noderankcalc(self.mode, self.case, True)
 #
 #    def test_graphreduce(self):
 #        reducegraph(self.mode, self.case, True)
